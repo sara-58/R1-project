79e2f9fe0fb4935f0af0d2f924c6568bf545d62a
Sara Mohamed <skyangelsara@gmail.com>
2024-01-21 23:52:59 +0200

    pro3

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,13 +1,10 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     You have recieved a New Email
 </head>
-
 <body>
     <div class="container" style=" width: 50vw;">
-
         <table class="table">
             <tr>
                 <td>From: </td>
@@ -15,15 +12,10 @@
             </tr>
             <tr>
                 <td>Email: </td>
-                <td>{{$email}}</td>
+                <td>{{$email}}</td><br>
             </tr>
         </table>
-
         <div style="border: 3px solid #ff6666; border-radius: 6px; padding: 30px 20px ">{{$messageContent}}</div>
-
-
     </div>
-
 </body>
-
 </html>
