c47c59e8c3100848326629d14892f3b78ddcb1c8
Sara Mohamed <skyangelsara@gmail.com>
2024-02-01 21:23:43 +0200

    pro9

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,7 +10,7 @@
             <div class="col-md-3 left_col">
                 <div class="left_col scroll-view">
                     <div class="navbar nav_title" style="border: 0;">
-                        <a href="users" class="site_title"><i class="fa fa-car"></i></i> <span>Rent Car Admin</span></a>
+                        <a href=" {{ route('users') }}" class="site_title"><i class="fa fa-car"></i> <span>Rent Car Admin</span></a>
                     </div>
                     <div class="clearfix"></div>
                     <!-- menu profile quick info -->
